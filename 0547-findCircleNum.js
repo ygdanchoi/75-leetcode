19b7b36304cf97f678c2680ec532864aa236044a
Dan Choi <ygdanchoi@gmail.com>
2020-04-03 10:38:49 -0400

    Integrate DisjointSets into 547

--- 0547-findCircleNum.js
+++ 0547-findCircleNum.js
@@ -31,3 +31,56 @@ function findCircleNum(M) {
     
     return groups;
 }
+
+function findCircleNum(M) {
+    const ds = new DisjointSets(getKeys(M.length));
+    
+    for (let i = 0; i < M.length; i++) {
+        for (let j = i + 1; j < M.length; j++) {
+            if (M[i][j] === 1 && !ds.areSameSet(i, j)) {
+                ds.merge(i, j);    
+            }
+        }
+    }
+    
+    return ds.numSets();
+}
+
+function getKeys(n) {
+    const keys = new Array(n);
+    
+    for (let i = 0; i < n; i++) {
+        keys[i] = i;
+    }
+    
+    return keys;
+}
+
+class DisjointSets {
+    constructor(keys) {
+        this.reps = new Map();
+        for (const key of keys) {
+            this.reps.set(key, key);
+        }
+    }
+    
+    areSameSet(a, b) {
+        return this.reps.get(a) === this.reps.get(b);
+    }
+    
+    merge(a, b) {
+        const aRep = this.reps.get(a);
+        const bRep = this.reps.get(b);
+        
+        for (const key of this.reps.keys()) {
+            if (this.reps.get(key) === bRep) {
+                this.reps.set(key, aRep);
+            }
+        }
+    }
+    
+    numSets() {
+        const reps = this.reps.values();
+        return new Set(reps).size;
+    }
+}
