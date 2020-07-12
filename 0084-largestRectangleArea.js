af3335b7af43c0e98f727f91cb281acdf9f37cce
Dan Choi <ygdanchoi@gmail.com>
2020-07-12 14:57:03 -0400

    more efficient largestRectangleArea

--- 0084-largestRectangleArea.js
+++ 0084-largestRectangleArea.js
@@ -1,24 +1,32 @@
 // https://leetcode.com/problems/largest-rectangle-in-histogram/
 
 function largestRectangleArea(heights) {
-  let best = 0;
-  const stack = []
-  
-  for (let i = 0; i < heights.length; i++) {
-      const height = heights[i];
-      let leftI = i;
-      
-      while (stack.length > 0 && height < stack[stack.length - 1].height) {
-          leftI = stack.pop().i;
-      }
-      
-      stack.push({ height, i: leftI });
-      
-      for (const left of stack) {
-          const width = i - left.i + 1;
-          best = Math.max(best, width * left.height);
-      }
-  }
-  
-  return best;
+    let best = 0;
+    const stack = [{ height: 0, i: -1 }]
+
+    for (let i = 0; i < heights.length; i++) {
+        const height = heights[i];
+        
+        while (height < last(stack).height) {
+            step(i);
+        }
+        
+        stack.push({ height, i });
+    }
+
+    while (stack.length > 1) {
+        step(heights.length);
+    }
+
+    function step(i) {
+        const h = stack.pop().height;
+        const w = i - last(stack).i - 1;
+        best = Math.max(best, w * h);
+    }
+
+    return best;
+}
+
+function last(stack) {
+    return stack[stack.length - 1];
 }
