a3c65e5baa87037390e35806fd771f38acf8b3b2
Dan Choi <ygdanchoi@gmail.com>
2021-02-01 07:52:57 -0500

    Less crappy 0057 insert

--- 0057-insert.js
+++ 0057-insert.js
@@ -17,6 +17,43 @@ function insert(intervals, newInterval) {
     return result;
 }
 
+function insert(intervals, newInterval) {
+    let start = null;
+    let deleteCount = 0;
+    
+    for (let i = 0; i < intervals.length; i++) {
+        if (isOverlapping(intervals[i], newInterval)) {
+            newInterval = [
+                Math.min(intervals[i][0], newInterval[0]),
+                Math.max(intervals[i][1], newInterval[1])
+            ];
+            if (start === null) {
+                start = i;
+            }
+            deleteCount++;
+        }
+    }
+    
+    if (start === null) {
+        start = 0;
+        
+        while (start < intervals.length && intervals[start][0] < newInterval[0]) {
+            start++;
+        }
+    }
+        
+    intervals.splice(start, deleteCount, newInterval);
+    return intervals;
+}
+
+function isOverlapping(a, b) {
+    return a[0] <= b[0] && b[0] <= a[1]
+        || a[0] <= b[1] && b[1] <= a[1]
+        || b[0] <= a[0] && a[0] <= b[1]
+        || b[0] <= a[1] && a[1] <= b[1];
+}
+
+
 function isOverlapping(a, b) {
     return a[0] <= b[0] && b[0] <= a[1]
         || a[0] <= b[1] && b[1] <= a[1]
