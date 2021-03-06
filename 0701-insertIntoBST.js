bf244a20df6c3b1e1514f7ec9bfcd35ec3abc1d3
Dan Choi <ygdanchoi@gmail.com>
2021-03-05 21:12:24 -0500

    Update insertIntoBST

--- 0701-insertIntoBST.js
+++ 0701-insertIntoBST.js
@@ -32,3 +32,81 @@ function insertIntoBST(root, val) {
   
   return root;
 }
+
+function insertIntoBST(root, val) {
+    if (!root) {
+        return new TreeNode(val);
+    }
+    
+    if (val < root.val) {
+        root.left = insertIntoBST(root.left, val);
+    } else {
+        root.right = insertIntoBST(root.right, val);
+    }
+    
+    return root;
+}
+
+// with AVL rebalancing
+function insertIntoBST(root, val) {
+    if (!root) {
+        return new TreeNode(val);
+    }
+    
+    if (val < root.val) {
+        root.left = insertIntoBST(root.left, val);
+    } else {
+        root.right = insertIntoBST(root.right, val);
+    }
+
+    update(root);
+    
+    if (root.balance < -1) {
+        if (root.left.balance <= 0) {
+            return rotateRight(root);
+        } else {
+            root.left = rotateLeft(root.left)
+            return rotateRight(root);
+        }
+    } else if (root.balance > 1) {
+        if (root.right.balance >= 0) {
+            return rotateLeft(root); 
+        } else {
+            root.right = rotateRight(root.right);
+            return rotateLeft(root);   
+        }
+    } else {
+        return root;
+    }
+}
+
+function rotateLeft(node) {
+    const right = node.right;
+    node.right = right.left;
+    right.left = node;
+    
+    update(node);
+    update(right);
+    return right;
+}
+
+function rotateRight(node) {
+    const left = node.left;
+    node.left = left.right;
+    left.right = node;
+    
+    update(node);
+    update(left);
+    return left;
+}
+
+function update(node) {
+    const lh = node.left
+        ? node.left.height
+        : 0;
+    const rh = node.right
+        ? node.right.height
+        : 0;
+    node.balance = rh - lh;
+    node.height = 1 + Math.max(lh, rh);
+}
