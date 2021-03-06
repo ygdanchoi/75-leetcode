ac24156d679686a51a1b2ffc098593a4e112b08a
Dan Choi <ygdanchoi@gmail.com>
2021-03-05 21:14:45 -0500

    701 indentation

--- 0701-insertIntoBST.js
+++ 0701-insertIntoBST.js
@@ -1,36 +1,36 @@
 // https://leetcode.com/problems/insert-into-a-binary-search-tree/
 
 function insertIntoBST(root, val) {
-  const LEFT = -1;
-  const RIGHT = 1;
+    const LEFT = -1;
+    const RIGHT = 1;
+    
+    let prev = null;
+    let facing = 0;
+    let curr = root;
+    
+    while (curr) {
+        prev = curr;
+        
+        if (val < curr.val) {
+            curr = curr.left;
+            facing = LEFT;
+        } else {
+            curr = curr.right;
+            facing = RIGHT;
+        }
+    }
+    
+    const node = new TreeNode(val);
+    
+    if (facing === LEFT) {
+        prev.left = node;
+    } else if (facing === RIGHT) {
+        prev.right = node;
+    } else {
+        return node;
+    }
   
-  let prev = null;
-  let facing = 0;
-  let curr = root;
-  
-  while (curr) {
-      prev = curr;
-      
-      if (val < curr.val) {
-          curr = curr.left;
-          facing = LEFT;
-      } else {
-          curr = curr.right;
-          facing = RIGHT;
-      }
-  }
-  
-  const node = new TreeNode(val);
-  
-  if (facing === LEFT) {
-      prev.left = node;
-  } else if (facing === RIGHT) {
-      prev.right = node;
-  } else {
-      return node;
-  }
-  
-  return root;
+    return root;
 }
 
 function insertIntoBST(root, val) {
